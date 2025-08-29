737121f5f64b1ec2bca262dc9e98dc6694a12480
AIWASS23 <marcelodearaujo677@gmail.com>
2025-08-29 08:16:23 -0300

    Initial Commit

---
+++ CallRTC/CallRTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallRTCApp.swift
+//  CallRTC
+//
+//  Created by Marcelo deAraújo on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CallRTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
